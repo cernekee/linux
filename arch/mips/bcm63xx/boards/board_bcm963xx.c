c49bcf0dd742ac8dcf0e7badd754055f60d53d8f
Maxime Bizon <mbizon@freebox.fr>
2011-05-24 21:47:41 +0200

    MIPS: BCM63XX: register ohci device.

--- arch/mips/bcm63xx/boards/board_bcm963xx.c
+++ arch/mips/bcm63xx/boards/board_bcm963xx.c
@@ -25,6 +25,7 @@
 #include <bcm63xx_dev_hsspi.h>
 #include <bcm63xx_dev_pcmcia.h>
 #include <bcm63xx_dev_spi.h>
+#include <bcm63xx_dev_usb_ohci.h>
 #include <bcm63xx_dev_usb_usbd.h>
 #include <board_bcm963xx.h>
 
@@ -898,6 +899,9 @@ int __init board_register_devices(void)
 
 	if (board.has_usbd)
 		bcm63xx_usbd_register(&board.usbd);
+
+	if (board.has_ohci0)
+		bcm63xx_ohci_register();
 
 	if (board.has_dsp)
 		bcm63xx_dsp_register(&board.dsp);
