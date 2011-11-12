8619b24a8c523d60747a4f7d183aa2022d6668ac
Jonas Gorski <jonas.gorski@gmail.com>
2011-11-12 12:18:26 +0100

    MIPS: BCM63XX: add HS SPI platform device and register it
    Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>

--- arch/mips/bcm63xx/boards/board_bcm963xx.c
+++ arch/mips/bcm63xx/boards/board_bcm963xx.c
@@ -22,6 +22,7 @@
 #include <bcm63xx_dev_enet.h>
 #include <bcm63xx_dev_dsp.h>
 #include <bcm63xx_dev_flash.h>
+#include <bcm63xx_dev_hsspi.h>
 #include <bcm63xx_dev_pcmcia.h>
 #include <bcm63xx_dev_spi.h>
 #include <bcm63xx_dev_usb_usbd.h>
@@ -913,6 +914,7 @@ int __init board_register_devices(void)
 			pr_err(PFX "failed to register fallback SPROM\n");
 	}
 #endif
+	bcm63xx_hsspi_register();
 
 	bcm63xx_spi_register();
 
---
+++ arch/mips/include/asm/mach-bcm63xx/bcm63xx_dev_hsspi.h
@@ -0,0 +1,20 @@
+#ifndef BCM63XX_DEV_HSSPI_H
+#define BCM63XX_DEV_HSSPI_H
+
+#include <linux/types.h>
+#include <bcm63xx_io.h>
+#include <bcm63xx_regs.h>
+
+int __init bcm63xx_hsspi_register(void);
+
+struct bcm63xx_hsspi_pdata {
+	int		bus_num;
+	u32		speed_hz;
+};
+
+#define bcm_hsspi_readl(o)	bcm_rset_readl(RSET_HSSPI, (o))
+#define bcm_hsspi_writel(v, o)	bcm_rset_writel(RSET_HSSPI, (v), (o))
+
+#define HSSPI_PLL_HZ_6328	133333333
+
+#endif /* BCM63XX_DEV_HSSPI_H */
